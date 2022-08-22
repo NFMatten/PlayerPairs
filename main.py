2947f3b5e9b408953e2fd5bf39186530c1c3cc52
Nikolaus Matten <nfmatten@gmail.com>
2022-08-22 10:38:19 -0600

    Test commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+def build_deck():
+    individual_items_in_suit = ["Ace", '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+
+    for items in individual_items_in_suit:
+        
+    return deck
+
+    # deck needs 4 of each card
+    # shuffle deck of 52 randomly 
+    # hand = 5 cards, needs 4 hands
+    # determine number of pairs in each hand
+    # display each hand, display number of pairs
+    # who won or tied
